cec58e2a69acae18e34bfd1fab45edf91576beae
ISOR3X <isorex@icloud.com>
2026-03-30 12:40:05 +0200

    feat(SectionWorker_FavColor): make responsive

--- src/PawnEditor/Sections/Bio/SectionWorker_FavColor.cs
+++ src/PawnEditor/Sections/Bio/SectionWorker_FavColor.cs
@@ -24,18 +24,4 @@ public class SectionWorker_FavColor(SectionDef def) : SectionWorker(def)
             Verse.Widgets.DrawRectFast(r.ContractedBy(GenUI.GapTiny), pawn.story.favoriteColor?.color ?? Color.white);
         });
     }
-
-    private static void DoFavColorInputRect(Rect inRect, Pawn pawn)
-    {
-        inRect = inRect.TakeTopPart(UIUtility.ButtonHeight);
-        var oldColor = pawn.story.favoriteColor?.color ?? Color.white;
-        var favColorRect = inRect.TakeRightPart(WidgetRow.IconSize).CenteredVertically(WidgetRow.IconSize);
-
-        Verse.Widgets.DrawLightHighlight(favColorRect);
-        favColorRect = favColorRect.ContractedBy(2f);
-        Verse.Widgets.DrawRectFast(favColorRect, pawn.story.favoriteColor?.color ?? Color.white);
-        inRect.xMax -= 2f;
-        if (Verse.Widgets.ButtonText(inRect, "Choose color"))
-            Find.WindowStack.Add(new Dialog_ColorPicker(c => pawn.story.favoriteColor?.color = c, oldColor));
-    }
 }
