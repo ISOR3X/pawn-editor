bb0eaa8c0814963cdfd93b96593758bca9466dba
ISOR3X <isorex@icloud.com>
2026-04-19 23:16:24 +0200

    fix(SectionWorker_Sex): restore icon

--- src/PawnEditor/Sections/Appearance/SectionWorker_Sex.cs
+++ src/PawnEditor/Sections/Appearance/SectionWorker_Sex.cs
@@ -1,7 +1,4 @@
-using Taffy;
 using Verse;
-using Void;
-using Void.Components;
 using Void.XMLComponents;
 using Layout = Void.Layout;
 
@@ -13,13 +10,6 @@ public class SectionWorker_Sex(SectionDef def) : SectionWorker(def)
     {
         layout.ComponentById<TextElement>("text").Content = "Sex";
         layout.ComponentById<ButtonElement>("button").Label = pawn.gender.GetLabel().CapitalizeFirst();
+        layout.ComponentById<ButtonElement>("button").Icon = pawn.gender.GetIcon();
     }
-    
-    // protected override void DoSectionContents(TaffyBuilder builder, Pawn pawn)
-    // {
-    //     builder.Text("Sex",
-    //         style: new StyleOverride { margin = new Rect<LengthPercentageAuto>(0f, GenUI.GapLabel, 0f, 0f) });
-    //     builder.Button(pawn.gender.GetLabel().CapitalizeFirst(), pawn.gender.GetIcon(), onClick: _ => { },
-    //         style: new StyleOverride { minWidth = 100f, maxWidth = 300f, flexGrow = 1f, width = Dimension.AUTO });
-    // }
 }
