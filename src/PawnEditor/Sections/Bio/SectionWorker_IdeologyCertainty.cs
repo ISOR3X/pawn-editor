138f141f58fb2e8b58e10fa62cb32c684e58e8f1
ISOREX <ISOREX@icloud.com>
2026-04-27 15:05:27 +0200

    chore(input): convert ideo certainty to new input as well

--- src/PawnEditor/Sections/Bio/SectionWorker_IdeologyCertainty.cs
+++ src/PawnEditor/Sections/Bio/SectionWorker_IdeologyCertainty.cs
@@ -12,17 +12,14 @@ public class SectionWorker_IdeologyCertainty(SectionDef def) : SectionWorker(def
 {
     public override void OnLayout(Layout layout, Pawn pawn)
     {
-        var cert = pawn.ideo.certaintyInt;
         var minMaxCert = new FloatRange(0, 1);
         layout.ComponentById<TextElement>("min").Content = minMaxCert.min.ToStringPercent();
         layout.ComponentById<TextElement>("max").Content = minMaxCert.max.ToStringPercent();
         layout.ComponentById<TextElement>("value").Content = pawn.ideo.Certainty.ToStringPercent();
-        layout.ComponentById<DivElement>("certainty").Children = b =>
-        {
-            b.InputRange(ref cert, minMaxCert.min, minMaxCert.max, step: 0.05f,
-                style: new StyleOverride { width = Dimension.Percent(1f) });
-            if (!Mathf.Approximately(cert, pawn.ideo.certaintyInt))
-                pawn.ideo.certaintyInt = cert;
-        };
+
+        var slider = layout.ComponentById<InputElement>("certainty");
+        slider.Value = new Reactive<float>(() => pawn.ideo.certaintyInt, v => pawn.ideo.certaintyInt = v);
+        slider.MinMax = minMaxCert;
+        slider.Step = 0.05f;
     }
 }
--- src/Void/XMLComponents/Input.cs
+++ src/Void/XMLComponents/Input.cs
@@ -8,6 +8,8 @@ public class InputElement : XMLComponent
 {
     public Ref? Value;
     public FloatRange? MinMax;
+    public float? Step;
+
     private string _inputType = "text";
 
     public override void ParseXmlAttrs(XmlNode node)
@@ -25,8 +27,8 @@ public class InputElement : XMLComponent
 
     public override void Render(TaffyBuilder builder, Action<TaffyBuilder>? children)
     {
-        // Resolved min max.
-        var resMm =  MinMax ?? new FloatRange(0, 9999);
+        var resMinMax = MinMax ?? new FloatRange(0, 9999);
+        var resStep = Step ?? 1f;
         switch (_inputType)
         {
             case "text" when Value is Ref<string> s:
@@ -38,17 +40,20 @@ public class InputElement : XMLComponent
                 break;
 
             case "range" when Value is Ref<float> f:
-                builder.InputRange(ref f.Inner, min: resMm.min, max: resMm.max, style: Style);
+                builder.InputRange(ref f.Inner, min: resMinMax.min, max: resMinMax.max, step: resStep, style: Style);
                 break;
             case "range" when Value is Ref<int> i:
-                builder.InputRange(() => i.Inner, v => i.Inner = (int)v, min: resMm.min, max: resMm.max, style: Style);
+                builder.InputRange(() => i.Inner, v => i.Inner = (int)v, min: resMinMax.min, max: resMinMax.max,
+                    step: resStep, style: Style);
                 break;
 
             case "range" when Value is Reactive<float> f:
-                builder.InputRange(() => f.Inner, v => f.Inner = v, min: resMm.min, max: resMm.max, style: Style);
+                builder.InputRange(() => f.Inner, v => f.Inner = v, min: resMinMax.min, max: resMinMax.max,
+                    step: resStep, style: Style);
                 break;
             case "range" when Value is Reactive<int> i:
-                builder.InputRange(() => i.Inner, v => i.Inner = (int)v, min: resMm.min, max: resMm.max, style: Style);
+                builder.InputRange(() => i.Inner, v => i.Inner = (int)v, min: resMinMax.min, max: resMinMax.max,
+                    step: resStep, style: Style);
                 break;
         }
     }
