79a972157c1a723ca019a88f3ec2cc36e4ee2f4c
ISOREX <ISOREX@icloud.com>
2026-06-02 16:57:45 +0200

    chore: conditional rendering and style setting helpers

--- src/PawnEditor/Windows/Window_Benchmark.cs
+++ src/PawnEditor/Windows/Window_Benchmark.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Linq;
 using Taffy;
 using UnityEngine;
@@ -32,26 +33,74 @@ public static class TaffyHelper
                 case TaffyMeasureMode.Exact or TaffyMeasureMode.FitContent:
                     return new TaffySize { width = width, height = Text.CalcHeight(context.Value.text, width) };
                 case TaffyMeasureMode.MinContent:
-                {
-                    var minW = context.Value.text.Split(' ').Select(w => Text.CalcSize(w).x).Prepend(0f).Max();
-                    return new TaffySize { width = minW, height = Text.CalcHeight(context.Value.text, minW) };
-                }
+                    {
+                        var minW = context.Value.text.Split(' ').Select(w => Text.CalcSize(w).x).Prepend(0f).Max();
+                        return new TaffySize { width = minW, height = Text.CalcHeight(context.Value.text, minW) };
+                    }
                 default:
-                {
-                    var sz = Text.CalcSize(context.Value.text);
-                    return new TaffySize { width = sz.x, height = sz.y };
-                }
+                    {
+                        var sz = Text.CalcSize(context.Value.text);
+                        return new TaffySize { width = sz.x, height = sz.y };
+                    }
             }
         }
     }
+
+    extension<T>(TaffyTree<T> tree) where T : struct
+    {
+        public TaffyNode NewNodeWithStyle(Action<TaffyStyleRef> configure, TaffyNode[]? children = null)
+        {
+            var node = tree.NewNode();
+            configure(tree.GetStyle(node));
+            if (children == null) return node;
+            foreach (var child in children)
+                tree.AppendChild(node, child);
+            return node;
+        }
+
+        public ConditionalNode<T> NewConditionalChild(TaffyNode parent, Action<TaffyStyleRef> configure, bool startVisible = true)
+        {
+            var content = tree.NewNodeWithStyle(configure);
+            if (startVisible)
+                tree.AppendChild(parent, content);
+            return new ConditionalNode<T>(tree, parent, content);
+        }
+    }
+}
+
+public class ConditionalNode<T> where T : struct
+{
+    private readonly TaffyTree<T> _tree;
+    private readonly TaffyNode _parent;
+    private readonly TaffyNode _content;
+
+    internal ConditionalNode(TaffyTree<T> tree, TaffyNode parent, TaffyNode content)
+    {
+        _tree = tree;
+        _parent = parent;
+        _content = content;
+    }
+
+    public bool IsVisible => _tree.GetParent(_content).HasValue;
+    public TaffyLayout GetParentLayout() => _tree.GetLayout(_parent);
+    public TaffyLayout GetContentLayout() => _tree.GetLayout(_content);
+
+    public void SetVisible(bool visible)
+    {
+        if (visible == IsVisible) return;
+        if (visible) _tree.AppendChild(_parent, _content);
+        else _tree.RemoveChild(_parent, _content);
+    }
+
+    public void Toggle() => SetVisible(!IsVisible);
 }
 
 public class Window_Benchmark : Window
 {
     private readonly TaffyTree<TaffyHelper.TaffyContext> _tree;
     private readonly TaffyNode _rootNode;
-    private TaffyNode _dynNode;
-    private TaffyNode _contentNode;
+    private readonly TaffyNode _buttonNode;
+    private readonly ConditionalNode<TaffyHelper.TaffyContext> _conditional;
 
     private const string LoremIpsum =
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor " +
@@ -66,40 +115,37 @@ public class Window_Benchmark : Window
         _tree = new TaffyTree<TaffyHelper.TaffyContext>();
 
         var textNode = _tree.NewLeafWithContext(new TaffyHelper.TaffyContext
-            { font = GameFont.Small, text = LoremIpsum });
+        { font = GameFont.Small, text = LoremIpsum });
 
-        var tempRoot = _tree.NewNode();
-        var rootStyle = _tree.GetStyle(tempRoot);
-        rootStyle.Display = TaffyDisplay.Flex;
-        rootStyle.FlexDirection = TaffyFlexDirection.Column;
-        rootStyle.Width = Dimension.Percent(1f);
-        rootStyle.Height = Dimension.Auto();
+        _rootNode = _tree.NewNodeWithStyle(s =>
+        {
+            s.Display = TaffyDisplay.Flex;
+            s.FlexDirection = TaffyFlexDirection.Column;
+            s.Width = Dimension.Percent(1f);
+            s.Height = Dimension.Auto();
+        }, children: [textNode]);
 
-        _rootNode = _tree.NewWithChildren(rootStyle, [textNode]);
-        _tree.RemoveNode(tempRoot);
+        var dynNode = _tree.NewNodeWithStyle(s =>
+        {
+            s.Width = Dimension.Px(200f);
+            s.Height = Dimension.Auto();
+            s.FlexDirection = TaffyFlexDirection.Column;
+        });
+        _tree.AppendChild(_rootNode, dynNode);
 
-        _dynNode = _tree.NewNode();
-        // GetStyle returns a reference, so no need to use SetStyle unless we want to also mark dirty.
-        var ds = _tree.GetStyle(_dynNode);
-        ds.Width = Dimension.Px(200f);
-        ds.Height = Dimension.Auto();
-        ds.FlexDirection = TaffyFlexDirection.Column;
+        _buttonNode = _tree.NewNodeWithStyle(s =>
+        {
+            s.Width = Dimension.Px(200f);
+            s.Height = Dimension.Px(30f);
+        });
+        _tree.AppendChild(dynNode, _buttonNode);
 
-        _tree.AppendChild(_rootNode, _dynNode);
-
-        var buttonNode = _tree.NewNode();
-        _tree.AppendChild(_dynNode, buttonNode);
-        var bs = _tree.GetStyle(buttonNode);
-        bs.Width = Dimension.Px(200f);
-        bs.Height = Dimension.Px(30f);
-        _tree.SetStyle(buttonNode, bs);
-
-        _contentNode = _tree.NewNode();
-        _tree.AppendChild(_dynNode, _contentNode);
-        var cs = _tree.GetStyle(_contentNode);
-        cs.MinWidth = Dimension.Px(400f);
-        cs.Width = Dimension.Percent(1f);
-        cs.Height = Dimension.Px(200f);
+        _conditional = _tree.NewConditionalChild(dynNode, s =>
+        {
+            s.MinWidth = Dimension.Px(400f);
+            s.Width = Dimension.Percent(1f);
+            s.Height = Dimension.Px(200f);
+        });
     }
 
     public override void Close(bool doCloseSound = true)
@@ -112,21 +158,16 @@ public class Window_Benchmark : Window
     {
         _tree.ComputeLayoutWithMeasure(_rootNode, inRect.width, inRect.height, TaffyHelper.MeasureText);
 
-        // Word wrapping
         var textNode = _tree.ChildAt(_rootNode, 0);
         var layout = _tree.GetLayout(textNode);
         var ctx = _tree.GetNodeContext(textNode);
         if (ctx == null) return;
 
-        // Conditional rendering
-        var dynLayout = _tree.GetLayout(_dynNode);
+        var dynLayout = _conditional.GetParentLayout();
         var dynOffset = inRect.position + new Vector2(dynLayout.x, dynLayout.y);
 
-        var btnNode = _tree.ChildAt(_dynNode, 0);
-        var btnLayout = _tree.GetLayout(btnNode);
-
         var r = layout.ToRect(inRect.position);
-        var r2 = btnLayout.ToRect(dynOffset);
+        var r2 = _tree.GetLayout(_buttonNode).ToRect(dynOffset);
 
         using (new TextBlock(ctx.Value.font, TextAnchor.UpperLeft))
         {
@@ -134,20 +175,10 @@ public class Window_Benchmark : Window
             Verse.Widgets.Label(r, ctx.Value.text);
         }
 
-        var contentAttached = _tree.GetParent(_contentNode).HasValue;
-        if (Verse.Widgets.ButtonText(r2, contentAttached ? "Hide" : "Show"))
-        {
-            if (contentAttached)
-                _tree.RemoveChild(_dynNode, _contentNode);
-            else
-                _tree.AppendChild(_dynNode, _contentNode);
-        }
+        if (Verse.Widgets.ButtonText(r2, _conditional.IsVisible ? "Hide" : "Show"))
+            _conditional.Toggle();
 
-        if (contentAttached)
-        {
-            var r3 = _tree.GetLayout(_contentNode).ToRect(dynOffset);
-            Verse.Widgets.DrawRectFast(r3, Color.blue);
-        }
-        
+        if (_conditional.IsVisible)
+            Verse.Widgets.DrawRectFast(_conditional.GetContentLayout().ToRect(dynOffset), Color.blue);
     }
 }
